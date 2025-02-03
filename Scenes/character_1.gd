acfc2e45c6a4a2d1778c25bf57150c7636bd1628
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-02-03 18:30:47 +0100

    viewport fix, health bar scales with players current health

--- Scenes/character_1.gd
+++ Scenes/character_1.gd
@@ -5,12 +5,12 @@ class_name character1 extends CharacterBody2D
 @export var maxhealth = 70
 var target = position
 
-
-
-
 func _input(event):
 	if event.is_action_pressed("click") and get_meta("active"):
 		target = get_global_mouse_position()
+	if event.is_action_pressed("Number 1 Character Selection") and get_meta("active"):
+		health -= 5 # ez csak teszt jelleggel van itt. ez alapján fogom a dead cuccot megcsinálni
+		
 		
 func _physics_process(_delta: float) -> void:
 	velocity = position.direction_to(target) * speed
